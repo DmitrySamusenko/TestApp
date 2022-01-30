37706fc54b25c56bbaa70a8dfe44ee75f16728f4
Dmitry Samusenko <royalitymean@gmail.com>
2022-01-31 00:46:33 +0300

    31January

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -15,7 +15,15 @@ class ViewController: UIViewController {
         //avatarImage.layer.borderWidth = 10.0
         //avatarImage.layer.borderColor = UIColor.lightGray.cgColor
     }
-
-
+    @IBAction func goID(_ sender: Any) {
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let newViewController = storyBoard.instantiateViewController(withIdentifier: "vc") as! NewViewController
+        self.present(newViewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func goVc(_ sender: Any) {
+        performSegue(withIdentifier: "goVc", sender: nil)
+    }
+    
 }
 
